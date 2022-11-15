6fb84396f118ebfce4382c9ed51cfc91e87cd20d
Grzegorz <grzesiek32reg@gmail.com>
2022-11-15 17:52:11 +0100

    webpack

---
+++ frontend/webpack.config.js
@@ -0,0 +1,47 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    mode:'development',
+    entry: {
+        main:path.resolve(__dirname,'src/index.js')  
+      },
+    output: {
+      path: path.resolve(__dirname, 'dist'),
+      filename: '[name].js',
+      assetModuleFilename: '[name][ext]',
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+          title: 'Dstreet',
+          filename: 'index.html',
+          template: path.resolve(__dirname, './src/template.html'),
+        }),
+      ],
+      module: {
+        rules: [
+          //css
+          { test: /\.css$/, use: ['style-loader', 'css-loader'] },
+          //images
+          { test: /\.(svg|ico|png|webp|jpg|gif|jpeg)$/, type: 'asset/resource' },
+          //js for babel
+          {
+            test: /\.js$/,
+            exclude: /node_modules/,
+            use: {
+              loader: 'babel-loader',
+              options: {
+                presets: ['@babel/preset-env'],
+              },
+            },
+          },
+        ],
+      },
+      devtool: 'inline-source-map',
+  devServer: {
+    static: path.resolve(__dirname, 'dist'),
+    port: 5001, //default 8080
+    open: true,
+    hot: true,
+  },
+  };
